a26cfc45e28aa0f063993134189c68c9c6cf19f3
Jorge Arbelaez <jearbelaezr@audisoft.com>
2019-10-26 19:18:58 -0500

    Agregado demo Angular

---
+++ ApiMTI/Controllers/ConsultaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiMTI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class ConsultaController : ControllerBase
+    {
+        private readonly ILogger<ConsultaController> _logger;
+
+        public ConsultaController(ILogger<ConsultaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+
+        [HttpPost]
+        public IEnumerable<string> Post()
+        {
+            return new string[] { "Hola Mundo Post" };
+        }
+    }
+}
---
+++ DemoSolid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DemoSolid
+{
+    class X
+    {
+        public string Nombre { get; set; }
+    }
+
+    class Y: X
+    {
+        public string Direccion { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            X x = new X();
+            Y y = new Y();
+            x = y; // Sustitución Liskov
+            y = (Y)x; // Cast tipo C++: Si la asignación es inválida genera InvalidCastException.
+            y = x as Y; // Cast estilo C#: Si la asignación es inválida devuelve nulo.
+            y.Direccion = "Calle 100";
+        }
+    }
+}
